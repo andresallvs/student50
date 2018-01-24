7484cec4ee3ad45bcae88ecc471d5858126669d2
André Alves <andrealves@MacBook-Pro-de-George.local>
2018-01-24 11:35:10 -0300

    add main routes

--- lecture0/app.py
+++ lecture0/app.py
@@ -3,7 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return 'INDEX'
 
+@app.route('/welcome')
+def sign_in():
+    return 'SIGN IN'
 
+@app.route('/who-are-you')
+def sign_up():
+    return 'SIGN UP'
+
+@app.route('/<username>')
+def user_profile(username):
+    return 'User %s PAGE' % username
+
+@app.route('/<username>/events')
+def user_events(username):
+    return 'USER %s EVENTS' % username
